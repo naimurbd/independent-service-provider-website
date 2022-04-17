ac1200f98b68ed3e31153b73cb5d2f0c4f75f6b3
naimurbd <naiemnaiem072@gmail.com>
2022-04-17 16:58:19 +0600

    Add One extra section on the home page

---
+++ src/Pages/Home/Experts/Experts.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import deliveryMan1 from '../../../images/delivery man/delivery-man1.png';
+import deliveryMan2 from '../../../images/delivery man/delivery-man2.png';
+import deliveryMan3 from '../../../images/delivery man/delivery-man3.png';
+import Expert from '../Expert/Expert';
+
+
+const deliveryMen = [
+    { id: 1, name: 'Shabaz Nadif', img: deliveryMan1 },
+    { id: 2, name: 'Rashid Khan', img: deliveryMan2 },
+    { id: 3, name: 'Munaf Hussain', img: deliveryMan3 },
+
+]
+
+const Experts = () => {
+    return (
+        <div className='container'>
+            <h2 className='text-primary text-center'>Our Delivery Man</h2>
+            <div className="row">
+                {
+                    deliveryMen.map(deliveryMan => <Expert
+                        key={deliveryMan.id}
+                        deliveryMan={deliveryMan}
+                    >
+
+                    </Expert>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Experts;
